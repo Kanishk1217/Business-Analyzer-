f35e1c6b51a1f0e396d54b8129faf10a8a051e01
Kanishk1217 <zesus680@gmail.com>
2026-01-03 15:06:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix
+import io
 # -----------------------------------------------------------------------------
 # 1. DARK UI SETUP
 # -----------------------------------------------------------------------------
